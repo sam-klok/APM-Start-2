7e36d938e20a83cb696d6a96e2cf3599cef1935a
sam-klok <sklok11@gmail.com>
2021-09-03 17:45:42 -0700

    initial commit

---
+++ readme.txt
@@ -0,0 +1,9 @@
+After all new stuff in Angular 12, I decided to go back to Angular course 
+"Angular" by Deborah Kurata from Pluralsight.
+
+So this application APM-Start... done to the middle of course. 
+I did it slightly differently from training.
+
+thank you,
+Sam Klok
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+  pageTitle = 'By Sam Klok';
+}
