ab860369d3c211b9a94df8b0bbe455dc28d980ba
sam klok <sklok11@gmail.com>
2021-09-08 13:03:45 -0700

    injecting service with data

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
 
@@ -31,38 +32,41 @@ export class ProductListComponent implements OnInit {
 
 
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      "productId":2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "1/1/2021",
-      "description": "15 gallons",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId":5,
-      "productName": "Hammer",
-      "productCode": "TBX-048",
-      "releaseDate": "10/1/2021",
-      "description": "Curved hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-  ];
+  products: IProduct[] = [];
+  // [
+  //   {
+  //     "productId":2,
+  //     "productName": "Garden Cart",
+  //     "productCode": "GDN-0023",
+  //     "releaseDate": "1/1/2021",
+  //     "description": "15 gallons",
+  //     "price": 32.99,
+  //     "starRating": 4.2,
+  //     "imageUrl": "assets/images/garden_cart.png"
+  //   },
+  //   {
+  //     "productId":5,
+  //     "productName": "Hammer",
+  //     "productCode": "TBX-048",
+  //     "releaseDate": "10/1/2021",
+  //     "description": "Curved hammer",
+  //     "price": 8.9,
+  //     "starRating": 4.8,
+  //     "imageUrl": "assets/images/hammer.png"
+  //   },
+  // ];
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     console.log("I'm in the ngOnInit now.");
-    this.listFilter = 'cart';
+    //this.listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   onRatingClicked(message: string): void {
